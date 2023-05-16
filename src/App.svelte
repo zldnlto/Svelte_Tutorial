4fac106018b18aee044e870b5ae1818c2950c7e3
Kimseolha <kynwww@naver.com>
2023-05-16 19:17:23 +0900

    4.1_{#if...}블록

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,15 @@
 <script>
-	export let name;
+	let name = 'universe';
+	let a = 2;
 </script>
 
 <main>
+	{#if a>0}
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
