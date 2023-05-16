5f49e1f57ec66aa501ba610e401bf5dd5044e889
Kimseolha <kynwww@naver.com>
2023-05-16 20:04:39 +0900

    Chp4_fetch 이용해 데이터 불러오기

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Chp4 from './tutorial/Chp4.svelte'
   import Chp4_2Promise from './tutorial/Chp4_2_promise.svelte';
+  import Chp4_3Fetch from './tutorial/Chp4_3_fetch.svelte';
 </script>
 
 <main>
 	<Chp4 name="world" a={3} b={-1}/>
 	<Chp4_2Promise />
+	<Chp4_3Fetch/>
 </main>
 
 <style>
---
+++ src/tutorial/Chp4_3_fetch.svelte
@@ -0,0 +1,45 @@
+<script>
+
+    async function getPosts(){
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const json = await res.json();
+        return json;
+    }
+    let promisePosts = getPosts();
+
+    
+</script>
+
+<main>
+    <button on:click={()=> promisePosts = getPosts()}>포스트 리스트 가져오기</button>
+
+    <table border="1">
+        <thead>
+            <th>id</th>
+            <th>사용자</th>
+            <th>타이틀</th>
+            <th>내용</th>
+        </thead>
+        <tbody>
+            {#await promisePosts}
+            <tr>
+                <td colspan=4>
+                    가져오는 중 잠시만 기다리세요 
+                </td>
+            </tr>
+
+            {:then posts}
+            {#each posts as post}
+            <tr>
+                <td>{post.id}</td>
+                <td>{post.userId}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+            </tr>
+            {/each}
+            {/await}
+        </tbody>
+
+    </table>
+
+</main>
