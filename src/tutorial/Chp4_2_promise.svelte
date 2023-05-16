5df006eda8db8e013973a36e836b29068dc33c59
Kimseolha <kynwww@naver.com>
2023-05-16 20:00:34 +0900

    Chp4_Promise와 {#await} 사용하기_버튼 연동

--- src/tutorial/Chp4_2_promise.svelte
+++ src/tutorial/Chp4_2_promise.svelte
@@ -1,18 +1,28 @@
 <script>
     async function promiseFunc(){
         const result = new Promise((resolve, reject) => {
-            setTimeout(()=> {resolve("하이")},3000)
+            setTimeout(()=> {
+                // resolve("하이")
+                reject(new Error("reject 입니다"))
+            
+            },3000)
     });
     
     const rtn = await result;
     return rtn;
 }
 
+let myPromise = promiseFunc(); //1. 최초 호출하기
+
 
 </script>
+<!-- 2. 버튼 만들어 호출하기 -->
+<button on:click={()=> myPromise = promiseFunc()}>async 함수 호출하기</button>
 
 <main>
-    {#await promiseFunc()}
+
+    <!-- 3. 비동기 함수 결과로 바꾸기 -->
+    {#await myPromise}
     <h1>로딩중..</h1>
     {:then res}
     <h1>{res}</h1>
