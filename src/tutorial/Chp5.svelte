08decd91a0f38b905c22061a5f44b9331a968438
Kimseolha <kynwww@naver.com>
2023-05-21 21:35:38 +0900

    Chp5_ 센서의 네가지 패턴

--- src/tutorial/Chp5.svelte
+++ src/tutorial/Chp5.svelte
@@ -13,4 +13,4 @@
   $: (센서) 뒤에 코드가 위치하는 데 이 코드 안에 반응형 변수(count)가 포함된
   실행 코드가 있어야 합니다.
 </p>
-<button on:click={addCount}>카운트는 {count}입니다</button>
+<button on:click={addCount}>카운트는 {count}입니다 </button>
---
+++ src/tutorial/Chp5_sensor.svelte
@@ -0,0 +1,34 @@
+<script>
+
+let count = 0;
+
+//센서 1. 한 줄 구문에 반응형 변수가 있을 떄 
+$: console.log("1. 한 줄 구문", count);
+
+//센서 2. 코드 블록 안에 반응형 변수가 있을 때
+$: {
+    count;
+    console.log("2. 코드 블록: count가 변경됨")
+}
+
+//센서 3. if 조건에 반응형 변수가 있을 때
+$: if(count>0) {
+    console.log("3. if조건: count가 0보다 크다.")
+}
+
+//센서 4. for loop 안에 반응형 변수가 있을 때 루프 실행
+$: for(let i =0; i<4; i++) {
+    count;
+    console.log("4. for loop 안에 count값 변경됨 for:", i)
+}
+
+function addCount(){
+    count = count +1; //트리거
+}
+
+
+</script>
+
+<main>
+    <button on:click={addCount}>카운트는 {count} 입니다</button>
+</main>
