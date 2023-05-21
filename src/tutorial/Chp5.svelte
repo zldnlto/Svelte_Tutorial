e4214e95e318106875984cc313d747a93b0cbf10
Kimseolha <kynwww@naver.com>
2023-05-21 20:36:36 +0900

    Chp5_ 할당을 통한 트리거 생성

--- src/tutorial/Chp5.svelte
+++ src/tutorial/Chp5.svelte
@@ -2,6 +2,10 @@
   let count = 10;
 
   $: console.log(`count는 ${count}입니다.`);
+
+  function addCount() {
+    count = count + 1;  //트리거
+  }
 </script>
 
 <p>센서 생성과 동작 정의</p>
@@ -9,3 +13,4 @@
   $: (센서) 뒤에 코드가 위치하는 데 이 코드 안에 반응형 변수(count)가 포함된
   실행 코드가 있어야 합니다.
 </p>
+<button on:click={addCount}>카운트는 {count}입니다</button>
