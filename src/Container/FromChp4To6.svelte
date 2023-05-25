981cc8f8f4edda44d1b2d68ba4e0a1801ee2b591
Kimseolha <kynwww@naver.com>
2023-05-25 21:19:02 +0900

    Chp4 ~ Chp6 정리

---
+++ src/Container/FromChp4To6.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Chp4_1 from '../Chp/Chp4_1.svelte';
+  import Chp4_2Promise from '../Chp/Chp4_2_promise.svelte';
+  import Chp4_3Fetch from '../Chp/Chp4_3_fetch.svelte';
+  import Chp4_4Key from '../Chp/Chp4_4_key.svelte';
+  import Chp4_5KeyLifecycle from '../Chp/Chp4_5_key_lifecycle.svelte';
+  import Chp4_6Html from '../Chp/Chp4_6_html.svelte';
+  import Chp5 from '../Chp/Chp5.svelte';
+  import Chp5Json from '../Chp/Chp5_JSON.svelte';
+  import Chp5Sensor from '../Chp/Chp5_sensor.svelte';
+  import Chp6 from '../Chp/Chp6.svelte';
+  import Chp6Each from '../Chp/Chp6_each.svelte';
+  import Chp6Contenteditable from '../Chp/Chp6_contenteditable.svelte';
+  import Chp6Select from '../Chp/Chp6_select.svelte';
+  import Chp6SelectExample from '../Chp/Chp6_select_example.svelte';
+  import Chp6Size from '../Chp/Chp6_size.svelte';
+  import Chp6This from '../Chp/Chp6_this.svelte';
+
+  let name = '얄루';
+</script>
+
+  <Chp4_1 name="world" a="{3}" b="{-1}" />
+  <Chp4_2Promise />
+  <!-- <Chp4_3Fetch/> -->
+  <Chp4_4Key />
+  
+  <input type="text" bind:value="{name}" />
+  {#key name}
+    <Chp4_5KeyLifecycle name="{name}" />
+  {/key}
+  
+  <Chp4_5KeyLifecycle name="{name}" />
+  <Chp4_6Html />
+  <Chp5Sensor />
+  <Chp5Json />
+  <Chp6 />
+  <Chp6SelectExample/>
+  <Chp6Contenteditable />
+  <Chp6Each />
+  <Chp6Size />
+  <Chp6This />
