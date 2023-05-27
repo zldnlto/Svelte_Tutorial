5db07fb7d2b2da466a25ba3ae38d251b9e58123d
Kimseolha <kynwww@naver.com>
2023-05-27 19:37:27 +0900

    Chp10_beforeUpdate, afterUpdate 예시"

--- src/Chp10/Child.svelte
+++ src/Chp10/Child.svelte
@@ -4,6 +4,7 @@
 
     onMount(()=> {
         console.log("Child onMount 호출됨"); 
+        
         document.getElementById("text01").focus();
         
         //onDestroy
@@ -15,7 +16,21 @@
     onDestroy(()=> {
         console.log("Child onDestroy 호출됨")
     })
+
+    beforeUpdate(()=> {
+        console.log("Child beforeUpdate 호출됨", x)
+
+    })
+
+    afterUpdate(()=> {
+        console.log("Child afterUpdate 호출됨", x)
+    })
+
+    let x = 0; 
 </script>
 
 <h1>Child</h1>
-<input type="text" id="text01">
+<input type="text" id="text01" bind:value={x}>
+<button on:click={()=> {
+    x += 1;
+}}></button>
