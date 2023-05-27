9f98b2d8f4a6ed490b320a3860871eeb69e91c03
Kimseolha <kynwww@naver.com>
2023-05-27 18:51:23 +0900

    Chp9_이벤트 포워딩하기

---
+++ src/Chp9/Content.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import SearchButton from "./SearchButton.svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function forward(event) {
+        //이벤트명, 데이터 모두 SearchButton과 같은 형태로 넘김
+        dispatch("searchClicked", evnet.detail)
+    }
+
+</script>
+
+
+<h2>Contents</h2>
+<!-- SearchButton 커스텀 이벤트 searchClicked의 이벤트 처리를 forward 함수로 연결  -->
+<SearchButton on:searchClicked={forward}/>
--- src/Container/Chp9.svelte
+++ src/Container/Chp9.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Content from "../Chp9/Content.svelte";
   import EventTutorial from "../Chp9/EventTutorial.svelte";
   import SearchButton from "../Chp9/SearchButton.svelte";
 
@@ -15,3 +16,9 @@ const customSearch = (event)=> {
 <SearchButton on:searchClicked={customSearch}/>
 <br>
 {searchText==""?"검색어를 입력 후 Search 버튼을 클릭하세요":"검색어: " + searchText }
+
+<!-- 이벤트 포워딩 -->
+<p>
+    <Content on:searchClicked={customSearch}/>
+    <br><br>{searchText==""?"검색어를 입력 후 Search 버튼을 클릭하세요":"검색어: " + searchText }
+</p>
