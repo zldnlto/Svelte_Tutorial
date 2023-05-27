b656806adfc42afb862216827c819ddb53c0681e
Kimseolha <kynwww@naver.com>
2023-05-27 19:21:44 +0900

    Chp10_onDestroy 예시

--- src/Chp10/Child.svelte
+++ src/Chp10/Child.svelte
@@ -5,7 +5,15 @@
     onMount(()=> {
         console.log("Child onMount 호출됨"); 
         document.getElementById("text01").focus();
+        
+        //onDestroy
+        return() => {
+            console.log("Child가 파괴되었습니다.")
+        }
+    })
 
+    onDestroy(()=> {
+        console.log("Child onDestroy 호출됨")
     })
 </script>
 
