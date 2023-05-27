5c5e358e12d4453751bfffd1b26891c0730ac1dc
Kimseolha <kynwww@naver.com>
2023-05-27 17:17:53 +0900

    Chp9_돔 이벤트 처리하기

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
   import Chp8 from "./Container/Chp8.svelte";
+  import Chp9 from "./Container/Chp9.svelte";
 import FromChp4To6 from "./Container/FromChp4To6.svelte";
   let name = '얄루';
 </script>
 
 <main>
   <!-- <FromChp4To6/> -->
-  <Chp8/>
+  <!-- <Chp8/> -->
+  <Chp9 />
 </main>
 
 <style>
---
+++ src/Container/Chp9.svelte
@@ -0,0 +1,21 @@
+<script>
+    let count = 0;
+    const search = (event) => {
+        console.log("조회 버튼 클릭")
+        console.log(event.target)
+        count = count +1;
+    }
+
+    const increase = () => {
+        console.log("increase");
+        count = count +1;
+    }
+</script>
+
+<button on:click={search} on:click={increase}>
+Search ({count})
+</button>
+
+<style>
+
+</style>
