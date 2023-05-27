8c9e5c7c872d9060176f9f136d1d46b0db6cf8bd
Kimseolha <kynwww@naver.com>
2023-05-27 18:57:36 +0900

    Chp9_돔 이벤트 포워딩하기

---
+++ src/Chp9/SimpleButton.svelte
@@ -0,0 +1,3 @@
+
+
+<button on:click>SimpleButton</button>
--- src/Container/Chp9.svelte
+++ src/Container/Chp9.svelte
@@ -2,12 +2,16 @@
   import Content from "../Chp9/Content.svelte";
   import EventTutorial from "../Chp9/EventTutorial.svelte";
   import SearchButton from "../Chp9/SearchButton.svelte";
+  import SimpleButton from "../Chp9/SimpleButton.svelte";
 
 let searchText = '';
 const customSearch = (event)=> {
     searchText = event.detail.text;
 }
 
+const simple = (event)=> {
+    alert("simple~!")
+}
 </script>
 
 
@@ -21,4 +25,9 @@ const customSearch = (event)=> {
 <p>
     <Content on:searchClicked={customSearch}/>
     <br><br>{searchText==""?"검색어를 입력 후 Search 버튼을 클릭하세요":"검색어: " + searchText }
+</p>
+
+<!-- 돔 포워딩 -->
+<p>
+    <SimpleButton on:click={simple}/>
 </p>
