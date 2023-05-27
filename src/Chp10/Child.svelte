600bbf35aabf01e3d4e870acf2e11c5d80d64b74
Kimseolha <kynwww@naver.com>
2023-05-27 19:19:26 +0900

    Chp10_onMount 예시

--- src/Chp10/Child.svelte
+++ src/Chp10/Child.svelte
@@ -1,2 +1,13 @@
+<script>
+    import {onMount, beforeUpdate, afterUpdate, onDestroy} from "svelte";
+
+
+    onMount(()=> {
+        console.log("Child onMount 호출됨"); 
+        document.getElementById("text01").focus();
+
+    })
+</script>
 
 <h1>Child</h1>
+<input type="text" id="text01">
