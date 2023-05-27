91773b92b8e576d725fd9c34bbba7bfce8265061
Kimseolha <kynwww@naver.com>
2023-05-27 20:56:34 +0900

    Chp11_readable 스토어 생성

--- src/Chp11/store.js
+++ src/Chp11/store.js
@@ -1,5 +1,17 @@
-import { writable } from 'svelte/store';
+import { writable, readable } from 'svelte/store';
 
 export const count = writable(10);
 
 console.log('스토어', count);
+
+export const time = readable(new Date(), function start(set) {
+  console.log('구독이 시작되었습니다');
+  const interval = setInterval(() => {
+    set(new Date());
+  }, 1000);
+
+  return function stop() {
+    console.log('모든 구독이 종료되었습니다');
+    clearInterval(interval);
+  };
+});
