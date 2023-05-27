bc81b404ea7e7cf5c0c12ffb498e8df2df01aa89
Kimseolha <kynwww@naver.com>
2023-05-27 19:09:33 +0900

    Chp10_라이프사이클 파트 코드 init

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Chp10 from "./Container/Chp10.svelte";
   import Chp8 from "./Container/Chp8.svelte";
   import Chp9 from "./Container/Chp9.svelte";
 import FromChp4To6 from "./Container/FromChp4To6.svelte";
@@ -8,7 +9,8 @@ import FromChp4To6 from "./Container/FromChp4To6.svelte";
 <main>
   <!-- <FromChp4To6/> -->
   <!-- <Chp8/> -->
-  <Chp9 />
+  <!-- <Chp9 /> -->
+  <Chp10 />
 </main>
 
 <style>
---
+++ src/Container/Chp10.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Child from "../Chp10/Child.svelte";
+  let flag = false;
+
+    
+</script>
+
+<section>
+    <button on:click={()=> flag = !flag}>{flag == true? "Destroy Child!" : "Create Child!"}</button>
+
+    <p>
+        {#if flag}
+            <Child />
+        {/if}
+    </p>
+</section>
+
