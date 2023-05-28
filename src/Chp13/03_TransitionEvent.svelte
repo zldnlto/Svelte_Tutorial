27a4c58a5146ad202e999814a748b4913e34cb41
Kimseolha <kynwww@naver.com>
2023-05-28 21:22:07 +0900

    Chp13_Transition 이벤트 상태

---
+++ src/Chp13/03_TransitionEvent.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { fly } from "svelte/transition";
+
+    let visible = true;
+    let status = "이벤트 기다리는 중 ..";
+</script>
+
+<p>이벤트 상태: {status}</p>
+
+<label>
+    <input type="checkbox" bind:checked={visible}>
+    visible
+</label>
+
+{#if visible}
+    <p 
+        transition:fly="{{y:200, duration: 2000}}"
+        on:introstart="{()=> status = '등장 시작'}"
+        on:introend="{()=> status = '등장했어요'}"
+        on:outrostart="{()=> status = '퇴장 시작'}"
+        on:outroend="{()=> status = '퇴장했어요'}">
+        트랜지션 이벤트 처리하기
+    </p>
+
+{/if}
--- src/Container/Chp13.svelte
+++ src/Container/Chp13.svelte
@@ -1,6 +1,7 @@
 <script>
   import Transition from "../Chp13/01_Transition.svelte";
   import InOut from "../Chp13/02_InOut.svelte";
+  import TransitionEvent from "../Chp13/03_TransitionEvent.svelte";
 
 
 </script>
@@ -8,3 +9,4 @@
 <h1>Chp13_트랜지션</h1>
 <Transition />
 <InOut />
+<TransitionEvent />
